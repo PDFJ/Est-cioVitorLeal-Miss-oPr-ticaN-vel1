479d9c1d38ff247ae9196f9f69ef211df9318735
Vitor Leal <42897549+PDFJ@users.noreply.github.com>
2024-07-14 16:30:17 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+
+#Coleta dos digitos
+digito_1=int(input("Digite o primeiro número: "))
+digito_2=int(input("Digite o segundo número: "))
+
+#Soma, subtração, multiplicação e divisão dos digitos respectivamente + resultados
+soma=digito_1+digito_2
+print("Resultado da adição: "+str(soma))
+sub=digito_1-digito_2
+print("Resultado da subtração: "+str(sub))
+muti=digito_1*digito_2
+print("Resultado da subtração: "+str(muti))
+div=digito_1/digito_2
+print("Resultado da subtração: "+str(div))
